a17c435f07a2c4109b5eb43c0206fe5405af79d5
mytogetherteam <mytogetherteam@gmail.com>
2026-05-08 01:08:41 +0700

    feat: import foundation package to auth service for platform debugging utilities

--- lib/core/auth/auth_service.dart
+++ lib/core/auth/auth_service.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:flutter/foundation.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:shared_preferences/shared_preferences.dart';
